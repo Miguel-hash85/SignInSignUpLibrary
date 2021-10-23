b0ee58c7223445f35050347673b6b814dfc3d125
Aitor <aitorruizdegauna@gmail.com>
2021-10-23 18:11:53 +0200

    Interface Signable developed with javadoc

--- src/interfaces/Signable.java
+++ src/interfaces/Signable.java
@@ -6,12 +6,22 @@
 package interfaces;
 
 /**
- *
+ * Interface Signable with the methos to signUp and sigIn.
  * @author Aitor
  */
 public interface Signable {
-
+    /**
+     * Method signUp that receives a User and throws Exception.
+     * @param user
+     * @throws Exception 
+     */
     public void signUp(User user) throws Exception;
+    /**
+     * Method signUp that receives a User, returns a User and throws Exception.
+     * @param user
+     * @return
+     * @throws Exception 
+     */
     public User signIn(User user) throws Exception;
     
 }
