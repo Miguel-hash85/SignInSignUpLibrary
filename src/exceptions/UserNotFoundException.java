db6cccb9e313fd46a2a89741cffb4405361117d5
Aitor <aitorruizdegauna@gmail.com>
2021-10-23 18:51:20 +0200

    Exceptions Developed with javadoc

---
+++ src/exceptions/UserNotFoundException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+
+/**
+ * Class UserNotFoundException that is used when the signIn username it´s not found.
+ * @author Aitor
+ */
+public class UserNotFoundException extends Exception{
+     /**
+     * Method getErrorMessage that return the error message.
+     * @return String
+     */
+    public String getErrorMessage(){
+        return "User not found";
+    }
+}
