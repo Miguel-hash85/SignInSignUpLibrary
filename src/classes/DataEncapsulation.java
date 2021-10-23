0d44e57376c37df121ae595f211bc4b7e8e83fb9
Aitor <aitorruizdegauna@gmail.com>
2021-10-23 17:53:42 +0200

    Data Encapsulation developed

---
+++ src/classes/DataEncapsulation.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Aitor
+ */
+public class DataEncapsulation implements Serializable{
+    private User user;
+    private Message message;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+    
+    
+}
---
+++ src/classes/Message.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Aitor
+ */
+public enum Message {
+    SIGNUP,SIGNIN,OK,USER_NOTFOUND,INCORRECT_PASSWORD,EXISTING_USERNAME,CONNECTION_ERROR
+}
